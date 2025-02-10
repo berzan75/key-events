e2936b049f5478af6693ea4fd5d9e0d09a3ff512
berzan75 <issundee358@gmail.com>
2025-02-10 19:56:17 +0100

    Update keys.py

--- keys.py
+++ keys.py
@@ -41,6 +41,21 @@ while player_y < 600:
                 keys[2]=False
             if event.key == K_RIGHT:
                 keys[3]=False
+    if keys [0]:
+        if player_y > 0:
+            player_y -=7
 
+    if keys [1]:
+        if player_y < 530:
+            player_y +=5
+    if keys [2]:
+        if player_x > 0:
+            player_x -= 5
+    if keys[3]:
+        if player_x < 530:
+            player_x += 5
 
+    player_y += 5
+    sleep(0.05)
 
+print("game over!")
